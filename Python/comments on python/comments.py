6e57e32500c6ee0a0b4e5e87939a3daddb3f730d
shajidul-sobuj <sajidislam1405@gmail.com>
2026-03-20 02:30:59 +0600

    first commit

---
+++ Python/comments on python/comments.py
@@ -0,0 +1,2 @@
+#hello this is my 1st python program
+print("I'm Sobuj!")
---
+++ Python/comments on python/multiline_comments.py
@@ -0,0 +1,4 @@
+'''this is the way to make multiline comments on python
+and my name is sobuj, ja python diye run kora hocche'''
+
+print("Hello, I'm Sobuj!")
