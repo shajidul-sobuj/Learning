c42e3f5bd53627571d06bccc451a0cdd0391324a
shajidul-sobuj <sajidislam1405@gmail.com>
2026-08-29 11:16:52 +0600

    add String.js and let_and_const__variabls.js with initial implementations

---
+++ JavaScript/String.js
@@ -0,0 +1,19 @@
+// String
+let firstName = "Kazi";
+// console.log(firstName);
+
+let secondName = "Sobuj";
+// console.log(secondName);
+
+// let fullName = firstName+" "+secondName; // normal way
+
+// let fullName = `${firstName} ${secondName}`; // alada kore string creation er maddhome  (template letaral)
+
+let fullName = `${firstName} ${secondName} \nSavar, Dhaka`; 
+
+// console.log(fullName);
+
+let randomstring = 'Iam "Sobuj"' // evabeo likhte paro, but double "" boro stign and eki string er moddhe choto string er jonno use korte parbo na, er jonno `` or '' or "" alada alada use korte hobe 
+
+console.log(randomstring);
+
